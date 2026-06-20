17804b2f8aaec0e6ab5f20b2a4aef74b29820102
Archana <nishadarchana045@gmail.com>
2026-06-20 10:48:17 +0530

    first message

---
+++ DAY6/script.js
@@ -0,0 +1,6 @@
+var f=document.getElementById("first");
+console.log(f);
+f.innerHTML="hello";
+let para = document.getElementsByClassName
+let e = document.querySelector('div.second');
+console.log(e);
